79f257bb012d04513671b322936af4fd29b9866e
anzembani <anzembani@obornes.fr>
2026-01-15 11:09:20 +0100

    Update cp_ws_list in variables.tf to include new simulator FR*ORV*ROO10 and adjust existing entries for FR*ORV*A0014 and A0015 to use test environment URLs.

--- deploy/cs-simulator/sdbx/variables.tf
+++ deploy/cs-simulator/sdbx/variables.tf
@@ -17,8 +17,9 @@ variable "cp_ws_list" {
     ws_url = string
   }))
   default = [
-    { cp_id = "FR*ORV*A0014", ws_url = "wss://proxy.ocpp-proxy.test.oreve.com" },
-    #{ cp_id = "FR*ORV*A0015", ws_url = "wss://proxy.ocpp-proxy.prod.oreve.com" },
+    # { cp_id = "FR*ORV*A0014", ws_url = "wss://proxy.ocpp-proxy.test.oreve.com" },
+    { cp_id = "FR*ORV*ROO10", ws_url = "wss://proxy.ocpp-proxy.test.oreve.com" },
+    { cp_id = "FR*ORV*A0015", ws_url = "wss://proxy.ocpp-proxy.test.oreve.com" },
     #{ cp_id = "FR*ORV*A0016", ws_url = "wss://proxy.ocpp-proxy.prod.oreve.com" },
     #{ cp_id = "FR*ORV*A0017", ws_url = "wss://proxy.ocpp-proxy.prod.oreve.com" },
     #{ cp_id = "FR*ORV*B9019", ws_url = "wss://proxy.ocpp-proxy.prod.oreve.com" },
